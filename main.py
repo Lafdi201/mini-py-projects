f18d9fbcd206811fab90ac3a2a8727119044efd1
Lafdi201 <ziyad.lafdi@ensam.ac.ma>
2024-10-01 13:46:52 +0100

    printing slot machine pin result

--- main.py
+++ main.py
@@ -13,6 +13,16 @@ sym_count = {
     "D" : 10
 }
 
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i,column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end="|")
+            else:
+                print(column[row], end="")
+        print()
+
 def machine_spins(rows,col,symbols):
     all_symbols = []
     for symbol, sym_count in symbols.items():
